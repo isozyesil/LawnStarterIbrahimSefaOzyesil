a2a8a2f161d403bb7f3074b77be643c5bb7554dc
isozyesil <Laplace1907@>
2025-09-03 08:56:58 +0300

    Video Recording for Test Run and removing spaces

--- LawnStarterIbrahimSefaOzyesil/TestDataHelper.cs
+++ LawnStarterIbrahimSefaOzyesil/TestDataHelper.cs
@@ -8,11 +8,9 @@ namespace LawnStarterIbrahimSefaOzyesil.Helpers
         public static string FirstName => faker.Name.FirstName();
         public static string LastName => faker.Name.LastName();
         public static string Email => faker.Internet.Email();
-
         public static string RandomFullName() => faker.Name.FullName();
         public static string RandomLastName() => faker.Name.LastName();
         public static string RandomEmail() => faker.Internet.Email();
-
         public static string CardNumber => "4111111111111111";
         public static string Expiry => DateTime.Now.AddYears(1).ToString("MM/yy");
         public static string CVV => new Random().Next(100, 999).ToString();
@@ -22,4 +20,3 @@ namespace LawnStarterIbrahimSefaOzyesil.Helpers
         }       
     }
 }
-
